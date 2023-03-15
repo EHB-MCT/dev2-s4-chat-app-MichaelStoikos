283ac6c5fcd6b6a716a2bc641b15b8687296a408
Michael Stoikos <michael.stoikos@student.ehb.be>
2023-03-15 12:23:24 +0100

    les 15-03

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 "use strict";
 
 const chat = {
-    author: "yourName",
-    init() {
-    },
-    sendMessage() {
-    },
-    fetchMessages() {
-    },
-    renderMessage(message) {
-    }
-
-}
+	author: "yourName",
+	init() {},
+	sendMessage() {},
+	fetchMessages() {
+		fetch("https://dev2chat.onrender.com/messages")
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	},
+	renderMessage(message) {},
+};
 
 chat.init();
